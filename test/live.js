0f535b9f79cc4f1a8bf45f79c9e6322e2b2ac690
José Perigolo Neto <51827621+Jose-Perigolo@users.noreply.github.com>
2022-11-10 10:22:00 -0300

    Update live.js

--- test/live.js
+++ test/live.js
@@ -15,88 +15,39 @@ Seneca({ legacy: false })
     // debug: true,
     file: [__dirname + '/local-env.js;?'],
     var: {
-      $TANGOCARD_KEY: String,
-      $TANGOCARD_NAME: String,
-      $TANGOCARD_CUSTID: String,
-      $TANGOCARD_ACCID: String,
+      $VERCEL_USERTOKEN: String,
     }
   })
   .use('provider', {
     provider: {
-      tangocard: {
-        keys: {
-          key: { value: '$TANGOCARD_KEY' },
-          name: { value: '$TANGOCARD_NAME' },
-          cust: { value: '$TANGOCARD_CUSTID' },
-          acc: { value: '$TANGOCARD_ACCID' },
-        }
-      }
-    }
-  })
-  .use('../',{
-    fetch: Fetch,
-    entity: {
-      order: {
-        save: {
-          sendEmail: true,
-          sender: {
-            email: 'richard+tangocard.sender.01@ricebridge.com',
-            firstName: 'Sender',
-            lastName: ''
+        vercel: {
+          keys: {
+            usertoken: { value: '$VERCEL_USERTOKEN' },
           }
         }
-      }
     }
   })
+  .use('../')
   .ready(async function() {
     const seneca = this
 
-    console.log(await seneca.post('sys:provider,provider:tangocard,get:info'))
+    console.log(await seneca.post('sys:provider,provider:vercel,get:info'))
     
-    const brands = await seneca.entity("provider/tangocard/brand").list$({
-      country: 'IE', verbose: false
-    })
-    console.log('brands',brands.length)
-    // console.dir(brands,{depth:null})
-    
-    let customers = await seneca.entity("provider/tangocard/customer").list$()
-    console.log('customers', customers.length)
-    console.dir(customers,{depth:null})
-    
-    let orders = await seneca.entity('provider/tangocard/order').list$()
-    console.log('orders',orders.length)
+    const projectsList = await seneca.entity("provider/vercel/projects").list$()
+    console.log('projectsList',projectsList.length)
 
+    // const projectLoad = await seneca.entity("provider/vercel/projects").load$()
+    // console.log('projectLoad',projectLoad.id)
+
+    // const projectsSave = await seneca.entity("provider/vercel/projects").save$()
+    // console.log('projectsSave',projectsSave.length)
     
-    let mark = Math.random()+''
-    let utid = 'U768452'
-    
-    let order = seneca.entity('provider/tangocard/order').data$({
-      amount: 10,
-      // campaign: 'test01',
-      campaign: '',
-      emailSubject: 'subject '+mark,
-      etid: 'E000000',
-      externalRefID: seneca.util.Nid(),
-      message: 'msg '+mark,
-      notes: 'note '+mark,
-      recipient: {
-        email: 'richard+tangocard.test.01@ricebridge.com',
-        firstName: 'First',
-        lastName: ''
-      },
-      // sendEmail: true,
-      // sender: {
-      //   email: '',
-      //   firstName: '',
-      //   lastName: ''
-      // },
-      utid
-    })
 
     try {
-      order = await order.save$()
-      console.log('order',order)
+      createProject = await createProject.save$()
+      console.log('project',project)
     }
+
     catch(e) {
       console.log(e.message)
       console.log(e.status)
